2bbff248d2ba5fcc87f7fcd40b959d31437430f5
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2021-06-02 06:27:57 -0400

    v0.1.1 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## v0.1.1
+
+* Improvements
+  * Log helpful message on init
+  * Link to the BME388 data sheet in the Hexdoc
+
 ## v0.1.0
 
 Initial release
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule BMP3XX.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @source_url "https://github.com/mnishiguchi/bmp3xx"
 
   def project do
@@ -48,7 +48,10 @@ defmodule BMP3XX.MixProject do
         "CHANGELOG.md"
       ],
       licenses: ["Apache-2.0"],
-      links: %{"GitHub" => @source_url}
+      links: %{
+        "GitHub" => @source_url,
+        "BMP388 data sheet" => "https://www.mouser.com/pdfdocs/BST-BMP388-DS001-01.pdf"
+      }
     }
   end
 
